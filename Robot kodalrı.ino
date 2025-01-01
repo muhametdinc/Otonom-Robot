d254de6cced91fd88814c95d9dc378502d522d93
Muhammet <dincc544@gmail.com>
2025-01-01 14:38:01 +0300

    Update Robot kodalrı.ino

--- Robot kodalrı.ino
+++ Robot kodalrı.ino
@@ -1,16 +1,87 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-
-// LCD ekran nesnesi (I2C adres: 0x27, satır: 2, sütun: 16)
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-
+#include <WiFi.h>
+#include <WebServer.h>
 // Adafruit TCS34725 kütüphanesi dahil edilir (RGB renk sensörü için)
 #include <Adafruit_TCS34725.h>
-
+#include <QTRSensors.h>
+#include <LiquidCrystal_I2C.h>
 // Entegrasyon zamanı ve kazanç değerleri (Renk sensörü için tanımlanır)
 #define TCS34725_INTEGRATIONTIME_700MS 0xF6 // Entegrasyon zamanı (verilerin toplanma süresi)
 #define TCS34725_GAIN_1X 0x00 // Kazanç ayarı (düşük ışık için hassasiyet)
+
+// LCD ayarları
+LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C adresi: 0x27
+
+// Wi-Fi ayarları
+const char* ssid = "WiFi_ADI";       // Wi-Fi adı
+const char* password = "WiFi_SIFRESI"; // Wi-Fi şifresi
+
+// Web sunucusu
+WebServer server(80);
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public partial class MainForm : Form
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private async void SendCommand(int hedef)
+    {
+        string url = $"http://ESP32_IP/istasyon";
+        var values = new Dictionary<string, string> { { "hedef", hedef.ToString() } };
+        var content = new FormUrlEncodedContent(values);
+
+        try
+        {
+            var response = await client.PostAsync(url, content);
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show($"Hedef istasyon {hedef} ayarlandı!");
+            }
+            else
+            {
+                MessageBox.Show("İstek başarısız oldu!");
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Hata: {ex.Message}");
+        }
+    }
+
+    private void buttonIstasyon1_Click(object sender, EventArgs e)
+    {
+        SendCommand(1);
+    }
+
+    private void buttonIstasyon2_Click(object sender, EventArgs e)
+    {
+        SendCommand(2);
+    }
+
+    private void buttonIstasyon3_Click(object sender, EventArgs e)
+    {
+        SendCommand(3);
+    }
+
+    private void buttonIstasyon4_Click(object sender, EventArgs e)
+    {
+        SendCommand(4);
+    }
+
+    private void buttonGiris_Click(object sender, EventArgs e)
+    {
+        SendCommand(0); // Giriş istasyonu
+    }
+}
+
 
 // Motor pinlerinin tanımlanması
 int motorR1 = 3; // Sağ ön motor pini
@@ -24,27 +95,63 @@ const int uTrig = 7; // Ultrasonik sensör sinyal gönderme pini
 const int uEcho = 8; // Ultrasonik sensör echo pini (yansıyan sinyali algılar)
 
 // Çizgi takip sensörlerinin bağlı olduğu pinler
-int qtrPin[] = {9, 10, 11, 12, 13}; // Çizgi sensör pinleri dizi olarak tanımlanır
+int qtrPin[] = {9, 10, 11, 12, 13}; // Sensör pinleri
+int sensorSayisi = 5;               // Sensör sayısı
+int sensorDegerleri[5];             // Sensör değerlerini saklamak için dizi
 
 // Motor ve sensör ayarları
 int motorHizi = 150; // Motor hızı (PWM değeri)
 int engelAlgila = 15; // Engel algılama mesafesi (cm)
 
-// PID kontrol değişkenleri
-float kp = 1.0;  // Oransal kazanç
-float ki = 0.0;  // İntegral kazanç
-float kd = 0.5;  // Türev kazanç
-float integral = 0.0; // İntegral bileşeni için başlangıç değeri
-float lastError = 0.0; // Önceki hata değeri (PID için)
-
 // Adafruit TCS34725 nesnesi (Renk sensörü için)
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
 // Robotun başlangıç pozisyonu ve hedef istasyonu
-int mevcutIstasyon = 1; // Robotun bulunduğu istasyon
-int hedefIstasyon = 1; // Varsayılan olarak mevcut istasyon
+int mevcutKonum = 0; // Robotun başlangıç konumunu 0 olarak tanımlıyoruz.
+int hedef = 1; // Varsayılan olarak mevcut istasyon
 
 void setup() {
+  Serial.begin(115200);
+
+  // Wi-Fi bağlantısını başlat
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Wi-Fi bağlantısı bekleniyor...");
+  }
+  Serial.println("Wi-Fi'ye bağlandı!");
+
+  // Sunucuya gelen istekleri işle
+  server.on("/istasyon", HTTP_POST, []() {
+    if (server.hasArg("hedef")) {
+      hedefIstasyon = server.arg("hedef").toInt();
+      Serial.print("Hedef istasyon: ");
+      Serial.println(hedefIstasyon);
+
+      server.send(200, "text/plain", "Hedef istasyon ayarlandı!");
+    } else {
+      server.send(400, "text/plain", "Hedef parametresi eksik!");
+    }
+  });
+
+  // Sunucuyu başlat
+  server.begin();
+
+  Serial.begin(9600);
+
+  // LCD başlat
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Renk Algilaniyor");
+
+  // Renk sensörünü başlat
+  if (tcs.begin()) {
+    Serial.println("TCS34725 baglandi.");
+  } else {
+    Serial.println("TCS34725 baglanamadi!");
+    while (1); // Hata varsa dur
+  }
   // Motor pinlerini çıkış olarak ayarla
   pinMode(motorR1, OUTPUT);
   pinMode(motorR2, OUTPUT);
@@ -72,92 +179,137 @@ void setup() {
     while (1); // Sonsuz döngüye girerek sistemi durdur
   }
 
-  // LCD ekranı başlat
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Renk Sensoru...");
-  delay(2000);
-  lcd.clear();
-
+ 
   Serial.begin(9600);
 
 }
 
 void loop() {
+  uint16_t r, g, b, c;
+  float redNorm, greenNorm, blueNorm;
+
+  // Renk sensöründen değerleri oku
+  tcs.getRawData(&r, &g, &b, &c);
+
+  // Normalize edilmiş RGB değerlerini hesapla
+  redNorm = (float)r / c * 255.0;
+  greenNorm = (float)g / c * 255.0;
+  blueNorm = (float)b / c * 255.0;
+
+  // Algılanan rengi belirle
+  String detectedColor = detectColor(redNorm, greenNorm, blueNorm);
+
+  // LCD üzerinde göster
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Renk: ");
+  lcd.setCursor(6, 0);
+  lcd.print(detectedColor);
+
+  delay(1000); // 1 saniye bekle
+  
+  server.handleClient();
+
+  // Robot hareket mantığı burada yazılacak
+  if (hedefIstasyon != 0) {
+    Serial.print("Robot, istasyon ");
+    Serial.print(hedefIstasyon);
+    Serial.println(" yönünde hareket ediyor...");
+    // Hedef istasyona gitme kodlarını buraya ekleyin
+    delay(5000); // Simülasyon için bekleme
+    hedefIstasyon = 0; // Hedefe ulaştıktan sonra sıfırla
+  }
+  
   // Hedef istasyon bilgisi kontrol edilir
-  if (hedefIstasyon != mevcutIstasyon) {
-    istasyonaGit(hedefIstasyon); // Hedef istasyona gitmek için fonksiyon çağrılır
+  if (hedef != mevcutKonum) {
+  istasyonaGit(mevcutKonum, hedef);  // Hedef istasyona gitmek için fonksiyon çağrılır
   }
 
   delay(100); // Döngüyü stabilize etmek için gecikme
 }
 
-void istasyonaGit(int hedef) {
-  // İstasyonlar arasında hareket için izlenecek algoritma
-  if (mevcutIstasyon == 1 && hedef == 2) {
-    cizgiTakip("mavi");
-    cizgiTakip("sari");
-  } else if (mevcutIstasyon == 2 && hedef == 3) {
-    cizgiTakip("sari");
-    cizgiTakip("yesil");
-  } else if (mevcutIstasyon == 3 && hedef == 4) {
-    cizgiTakip("yesil");
-    cizgiTakip("turuncu");
-  } else if (mevcutIstasyon == 4 && hedef == 1) {
-    cizgiTakip("turuncu");
-    cizgiTakip("mavi");
-  } else if (mevcutIstasyon == 1 && hedef == 3) {
-    cizgiTakip("mavi");
-    cizgiTakip("sari");
-    cizgiTakip("yesil");
-  } else if (mevcutIstasyon == 2 && hedef == 4) {
-    cizgiTakip("sari");
-    cizgiTakip("yesil");
-    cizgiTakip("turuncu");
-  } else if (mevcutIstasyon == 3 && hedef == 1) {
-    cizgiTakip("yesil");
-    cizgiTakip("turuncu");
-    cizgiTakip("mavi");
-  } else if (mevcutIstasyon == 4 && hedef == 2) {
-    cizgiTakip("turuncu");
-    cizgiTakip("mavi");
-    cizgiTakip("sari");
-  }
-
-  mevcutIstasyon = hedef; // Yeni istasyonu mevcut olarak ayarla
-}  
-
-void cizgiTakip(String renk) {
-  Serial.print(renk); // Takip edilen çizginin rengini yazdır
-  Serial.println(" çizgisi takip ediliyor...");
-
-  while (true) {
-    int sensorValues[5]; // Çizgi sensör değerlerini saklamak için dizi
-    for (int i = 0; i < 5; i++) {
-      sensorValues[i] = digitalRead(qtrPin[i]); // Sensör değerlerini oku
+// Robotun istasyona gitmesini sağlayan fonksiyon
+void istasyonaGit(int mevcutKonum, int hedef) {
+  // Robot başlangıç pozisyonundaysa (istasyon dışı)
+  if (mevcutKonum == 0) {
+    if (hedef == 1) {
+      cizgiTakip("mavi");
+    } else if (hedef == 2) {
+      cizgiTakip("mavi");
+      cizgiTakip("sari");
+    } else if (hedef == 3) {
+      cizgiTakip("mavi");
+      cizgiTakip("sari");
+      cizgiTakip("yesil");
+    } else if (hedef == 4) {
+      cizgiTakip("mavi");
+      cizgiTakip("sari");
+      cizgiTakip("yesil");
+      cizgiTakip("turuncu");
     }
-
-    // Hata hesaplama (PID için)
-    float error = (sensorValues[0] * -2) + (sensorValues[1] * -1) + (sensorValues[3] * 1) + (sensorValues[4] * 2); // Sensörlerden gelen hatayı hesapla
-    integral += error; // İntegral değerine hatayı ekle
-    float derivative = error - lastError; // Türev bileşenini hesapla
-    float pid = (kp * error) + (ki * integral) + (kd * derivative); // PID denklemini uygula
-    lastError = error; // Son hatayı güncelle
-
-    // Motor hızlarını PID ile ayarla
-    int solHiz = motorHizi + pid; // Sol motor için hız
-    int sagHiz = motorHizi - pid; // Sağ motor için hız
-
-    motorControl(constrain(solHiz, 0, 255), constrain(sagHiz, 0, 255)); // Hızları sınırlayıp motor kontrol fonksiyonuna gönder
-
-    // Çizgi kaybolursa dur
-    if (sensorValues[0] == LOW && sensorValues[1] == LOW && sensorValues[2] == LOW && sensorValues[3] == LOW && sensorValues[4] == LOW) {
-      motorControl(0, 0); // Motorları durdur
-      break; // Döngüden çık
+  } else {
+    // Robot bir istasyondaysa
+    if (mevcutKonum == 1 && hedef == 2) {
+      cizgiTakip("sari");
+    } else if (mevcutKonum == 2 && hedef == 3) {
+      cizgiTakip("yesil");
+    } else if (mevcutKonum == 3 && hedef == 4) {
+      cizgiTakip("turuncu");
+    } else if (mevcutKonum == 4 && hedef == 1) {
+      cizgiTakip("mavi");
+    } else if (mevcutKonum == 1 && hedef == 3) {
+      cizgiTakip("sari");
+      cizgiTakip("yesil");
+    } else if (mevcutKonum == 2 && hedef == 4) {
+      cizgiTakip("yesil");
+      cizgiTakip("turuncu");
+    } else if (mevcutKonum == 3 && hedef == 1) {
+      cizgiTakip("turuncu");
+      cizgiTakip("mavi");
+    } else if (mevcutKonum == 4 && hedef == 2) {
+      cizgiTakip("mavi");
+      cizgiTakip("sari");
     }
   }
+
+  // Hedefe ulaşıldığında mevcut konumu güncelle
+  ::mevcutKonum = hedef;
 }
+
+void cizgiTakip(String hedefRenk) {
+    while (true) { // Çizgi üzerinde kalmak için sürekli kontrol
+        String algilananRenk = renkAlgila(); // Mevcut algılanan rengi oku
+        
+        if (algilananRenk == "Hata") {
+            Serial.println("Hata: Renk algılanamadı, robot durduruluyor.");
+            motorControl(0, 0); // Motorları durdur
+            return; // Hatalı durumda fonksiyondan çık
+        }
+
+        if (algilananRenk == hedefRenk) {
+            // Hedef renkteyiz, düz ilerle
+            motorControl(motorHizi, motorHizi);
+        } else if (algilananRenk == "Bilinmeyen") {
+            // Hedef renkten saptı, motorları durdur ve yeniden algıla
+            Serial.println("Renk bilinmiyor, tekrar algılama yapılıyor.");
+            motorControl(0, 0);
+        } else {
+            // Robot hedef renkten uzaklaşmış, düzeltici hareket yap
+            if (hedefRenk == "Mavi" || hedefRenk == "Yeşil") {
+                // Daha düşük renk tonları için sağa dönüş yap
+                sagDonus();
+            } else {
+                // Daha yüksek renk tonları için sola dönüş yap
+                solDonus();
+            }
+        }
+
+        // Algılama süreci devam ederken bir miktar bekle
+        delay(100); // Robotun tepki süresine göre bu değer ayarlanabilir
+    }
+}
+
+
 
 void motorControl(int solHiz, int sagHiz) {
   analogWrite(motorL1, solHiz); // Sol motor ileri hız
@@ -178,72 +330,96 @@ void solDonus() {
   motorControl(0, 0); // Dönüş sonrası dur
 }
 
-void renkAlgila() {
-  uint16_t r, g, b, c; // Renk sensörü ham verileri için değişkenler
-  tcs.getRawData(&r, &g, &b, &c); // Renk sensöründen ham değerleri oku
+String renkAlgila() {
+    uint16_t r, g, b, c; // Renk sensöründen gelen ham veriler için değişkenler
+    tcs.getRawData(&r, &g, &b, &c); // Renk sensöründen ham değerleri oku
 
-  float rNorm = (float)r / c; // Kırmızı renk oranı
-  float gNorm = (float)g / c; // Yeşil renk oranı
-  float bNorm = (float)b / c; // Mavi renk oranı
+    // Toplam aydınlık kontrolü
+    if (c == 0 || c < 100) { // Toplam renk değeri sıfır ya da düşükse hata mesajı döndür
+        Serial.println("Hata: Toplam renk değeri düşük. Algılama yapılamadı.");
+        return "Hata"; // Hata durumunda fonksiyondan çık
+    }
 
-  Serial.print("R: "); Serial.print(rNorm, 2); // Kırmızı oranını yazdır
-  Serial.print(" G: "); Serial.print(gNorm, 2); // Yeşil oranını yazdır
-  Serial.print(" B: "); Serial.println(bNorm, 2); // Mavi oranını yazdır
+    // Renk oranlarını hesapla (normalizasyon)
+    float rNorm = (float)r / c; // Kırmızı renk oranı
+    float gNorm = (float)g / c; // Yeşil renk oranı
+    float bNorm = (float)b / c; // Mavi renk oranı
 
-  if (rNorm > 0.4 && gNorm > 0.4 && bNorm < 0.2) {
-    Serial.println("Sarı renk algılandı!"); // Sarı renk algılandı
-  } else if (rNorm > 0.5 && gNorm < 0.3 && bNorm < 0.2) {
-    Serial.println("Turuncu renk algılandı!"); // Turuncu renk algılandı
-  } else if (bNorm > 0.5 && rNorm < 0.3 && gNorm < 0.3) {
-    Serial.println("Mavi renk algılandı!"); // Mavi renk algılandı
-  } else if (gNorm > 0.5 && rNorm < 0.3 && bNorm < 0.3) {
-    Serial.println("Yeşil renk algılandı!"); // Yeşil renk algılandı
-  } else {
-    Serial.println("Renk algılanamadı veya bilinmeyen renk."); // Renk tanımlanamadı
-  }
+    // Seri monitöre renk oranlarını yazdır
+    Serial.print("R: "); Serial.print(rNorm, 2); 
+    Serial.print(" G: "); Serial.print(gNorm, 2); 
+    Serial.print(" B: "); Serial.println(bNorm, 2);
+
+    // Algılanan rengi belirle
+    if (rNorm > 0.4 && gNorm > 0.4 && bNorm < 0.2) {
+        Serial.println("Sarı renk algılandı!"); // Sarı renk
+        return "Sarı";
+    } else if (rNorm > 0.5 && gNorm < 0.3 && bNorm < 0.2) {
+        Serial.println("Turuncu renk algılandı!"); // Turuncu renk
+        return "Turuncu";
+    } else if (bNorm > 0.5 && rNorm < 0.3 && gNorm < 0.3) {
+        Serial.println("Mavi renk algılandı!"); // Mavi renk
+        return "Mavi";
+    } else if (gNorm > 0.5 && rNorm < 0.3 && bNorm < 0.3) {
+        Serial.println("Yeşil renk algılandı!"); // Yeşil renk
+        return "Yeşil";
+    } else {
+        Serial.println("Renk algılanamadı veya bilinmeyen renk."); // Tanımsız renk
+        return "Bilinmeyen";
+    }
 }
+
+
 
 void engelKontrol() {
-  digitalWrite(uTrig, LOW); // Ultrasonik sensör trig pinini kapat
-  delayMicroseconds(2); // 2 mikro saniye bekle
-  digitalWrite(uTrig, HIGH); // Ultrasonik sensör trig pinini aç
-  delayMicroseconds(10); // 10 mikro saniye sinyal gönder
-  digitalWrite(uTrig, LOW); // Trig pinini kapat
+  digitalWrite(uTrig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(uTrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(uTrig, LOW);
 
-  long sure = pulseIn(uEcho, HIGH); // Echo pini yüksek olduğunda geçen süreyi ölç
-  int mesafe = sure / 58.2; // Ölçülen süreyi mesafeye çevir
+  long sure = pulseIn(uEcho, HIGH, 30000); // Maksimum 30ms bekle
+  if (sure == 0) return; // Yanıt yoksa fonksiyondan çık
+
+  int mesafe = sure / 58.2;
 
   if (mesafe < engelAlgila) {
-    Serial.println("Engel algılandı! Duruluyor..."); // Engel algılandığında mesaj yazdır
-    motorControl(0, 0); // Motorları durdur
+    Serial.println("Engel algılandı! Duruluyor...");
+    motorControl(0, 0);
+    while (true) {
+      delay(500); // Engel durumu sürekli kontrol edilir
+      digitalWrite(uTrig, LOW);
+      delayMicroseconds(2);
+      digitalWrite(uTrig, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(uTrig, LOW);
+
+      sure = pulseIn(uEcho, HIGH, 30000);
+      if (sure == 0) continue;
+
+      mesafe = sure / 58.2;
+      if (mesafe > engelAlgila) {
+        Serial.println("Engel kaldırıldı. Devam ediliyor...");
+        break; // Engel kalktığında döngüden çık
+      }
+    }
   }
 }
 
-void renkAlgila(float rNorm, float gNorm, float bNorm) {
-  lcd.clear();
-  lcd.setCursor(0, 0); // İlk satıra yaz
-
-  // Algılanan renk bilgisi
-  if (rNorm > 0.4 && gNorm < 0.3 && bNorm < 0.3) {
-    lcd.print("KIRMIZI Algilandi");
-  } else if (rNorm > 0.4 && gNorm > 0.4 && bNorm < 0.2) {
-    lcd.print("SARI Algilandi ");
-  } else if (bNorm > 0.5 && rNorm < 0.3 && gNorm < 0.3) {
-    lcd.print("MAVI Algilandi ");
-  } else if (gNorm > 0.5 && rNorm < 0.3 && bNorm < 0.3) {
-    lcd.print("YESIL Algilandi");
+/ Renk algılama fonksiyonu
+String detectColor(float r, float g, float b) {
+  if (r > g && r > b) {
+    return "Kirmizi";
+  } else if (g > r && g > b) {
+    return "Yesil";
+  } else if (b > r && b > g) {
+    return "Mavi";
+  } else if (r > 200 && g > 200 && b > 200) {
+    return "Beyaz";
+  } else if (r < 50 && g < 50 && b < 50) {
+    return "Siyah";
   } else {
-    lcd.print("Renk Algilanamadi");
+    return "Bilinmeyen";
   }
-
-  // İkinci satıra RGB oranlarını yaz
-  lcd.setCursor(0, 1);
-  lcd.print("R: ");
-  lcd.print(rNorm, 2);
-  lcd.print(" G: ");
-  lcd.print(gNorm, 2);
-  lcd.print(" B: ");
-  lcd.print(bNorm, 2);
-
-  delay(1000); // Ekranda gösterim süresi
 }
+
